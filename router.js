af85ad6fdf147c027238e3ac3fbb7173afa786a5
sriley <sarahriley.dev@gmail.com>
2019-06-11 16:21:55 -0400

    separating router from server

---
+++ router.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+
+router.post("*", async (req, res) => {
+  let { sessionId, serviceCode, phoneNumber, text } = req.body;
+  let response = "";
+  switch (text) {
+    case "":
+      response =
+        "CON Choose your country \n 1. BTI \n 2. DRC \n 3. KEN \n 4. MWI \n 5. RWA \n 6. SSD \n 7. TZA \n 8. UGA";
+      break;
+    case "1":
+      response =
+        "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
+      break;
+    case "1*1":
+      response =
+        "CON Choose your commodity \n 1. Animal Products \n 2. Beans \n 3. Cereals";
+      break;
+    case "1*1*1":
+      response =
+        "CON Choose your sub-category \n 1. Animal Products \n 2. Livestock \n 3. Poultry";
+      break;
+    case "1*1*1*1":
+      response =
+        "CON Choose your product \n 1. Eggs \n 2. Exotic Eggs \n 3. Local Eggs";
+      break;
+    case "1*1*1*1*1":
+      let sql = `
+        SELECT price
+
+  FROM products
+  WHERE country = 'BTI' AND market = 'Bujumbaru' AND product = 'beans';`;
+      try {
+        const results = await db.raw(sql);
+     
+        response = `END Current prices for \n Eggs ${results[0].price}`;
+      } catch (error) {
+        console.log(error);
+        // do stuff with error
+      }
+      // response = `END Current prices for \n Eggs ${prices}`;
+      break;
+    default:
+      response = "Bad request!";
+  }
+  res.send(response);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const db = require("./data/dbConfig");
 
+const router = require('./router');
+
 const port = process.env.PORT || 3030;
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+server.use('*', router);
 
 app.get("*", (req, res) => {
   res.send(
@@ -15,59 +18,57 @@ app.get("*", (req, res) => {
   );
 });
 
-function get() {
-  return db("products");
-}
 
-app.post("*", async (req, res) => {
-  let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  let accountNumber = "ACC1001";
-  let prices = "NGN 10,000";
-  let response = "";
-  switch (text) {
-    case "":
-      response =
-        "CON Choose your country \n 1. BTI \n 2. DRC \n 3. KEN \n 4. MWI \n 5. RWA \n 6. SSD \n 7. TZA \n 8. UGA";
-      break;
-    case "1":
-      response =
-        "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
-      break;
-    case "1*1":
-      response =
-        "CON Choose your commodity \n 1. Animal Products \n 2. Beans \n 3. Cereals";
-      break;
-    case "1*1*1":
-      response =
-        "CON Choose your sub-category \n 1. Animal Products \n 2. Livestock \n 3. Poultry";
-      break;
-    case "1*1*1*1":
-      response =
-        "CON Choose your product \n 1. Eggs \n 2. Exotic Eggs \n 3. Local Eggs";
-      break;
-    case "1*1*1*1*1":
-      let sql = `
-        SELECT price
 
-  FROM products
-  WHERE country = 'BTI' AND market = 'Bujumbaru' AND product = 'beans';`;
-      try {
-        const results = await db.raw(sql);
-        console.log(results);
-        console.log(results[0].price);
-        console.log(typeof results[0].price);
-        response = `END Current prices for \n Eggs ${results[0].price}`;
-      } catch (error) {
-        console.log(error);
-        // do stuff with error
-      }
-      // response = `END Current prices for \n Eggs ${prices}`;
-      break;
-    default:
-      response = "Bad request!";
-  }
-  res.send(response);
-});
+// app.post("*", async (req, res) => {
+//   let { sessionId, serviceCode, phoneNumber, text } = req.body;
+//   let accountNumber = "ACC1001";
+//   let prices = "NGN 10,000";
+//   let response = "";
+//   switch (text) {
+//     case "":
+//       response =
+//         "CON Choose your country \n 1. BTI \n 2. DRC \n 3. KEN \n 4. MWI \n 5. RWA \n 6. SSD \n 7. TZA \n 8. UGA";
+//       break;
+//     case "1":
+//       response =
+//         "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
+//       break;
+//     case "1*1":
+//       response =
+//         "CON Choose your commodity \n 1. Animal Products \n 2. Beans \n 3. Cereals";
+//       break;
+//     case "1*1*1":
+//       response =
+//         "CON Choose your sub-category \n 1. Animal Products \n 2. Livestock \n 3. Poultry";
+//       break;
+//     case "1*1*1*1":
+//       response =
+//         "CON Choose your product \n 1. Eggs \n 2. Exotic Eggs \n 3. Local Eggs";
+//       break;
+//     case "1*1*1*1*1":
+//       let sql = `
+//         SELECT price
+
+//   FROM products
+//   WHERE country = 'BTI' AND market = 'Bujumbaru' AND product = 'beans';`;
+//       try {
+//         const results = await db.raw(sql);
+//         console.log(results);
+//         console.log(results[0].price);
+//         console.log(typeof results[0].price);
+//         response = `END Current prices for \n Eggs ${results[0].price}`;
+//       } catch (error) {
+//         console.log(error);
+//         // do stuff with error
+//       }
+//       // response = `END Current prices for \n Eggs ${prices}`;
+//       break;
+//     default:
+//       response = "Bad request!";
+//   }
+//   res.send(response);
+// });
 
 // app.post("*", (req, res) => {
 //   let { sessionId, serviceCode, phoneNumber, text } = req.body;
