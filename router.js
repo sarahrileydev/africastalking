31662f8d8ec70ba82046dd4e1fd647fec0f5863f
sriley <sarahriley.dev@gmail.com>
2019-06-13 14:40:30 -0400

    ammend

--- router.js
+++ router.js
@@ -5,20 +5,19 @@ const responses = require("./responses");
 
 router.post("*", async (req, res) => {
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  
+
   let response = "";
   switch (text) {
     case "":
       response =
         "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
-        // console.log("sessionId 1", sessionId)
-        // console.log("serviceCode 1", serviceCode)
-        // console.log("phone Number 1", phoneNumber)
+      // console.log("sessionId 1", sessionId)
+      // console.log("serviceCode 1", serviceCode)
+      // console.log("phone Number 1", phoneNumber)
       break;
     case "1":
-      response =
-        responses.getProductsInMarket("Bujumbaru")
-      
+      response = await responses.getProductsInMarket("Bujumbaru");
+
       break;
     case "1*1":
       try {
@@ -30,7 +29,6 @@ router.post("*", async (req, res) => {
         newPrice.toString();
 
         response = `END Current prices for \n Eggs ${newPrice}`;
-      
       } catch (error) {
         console.log(error);
         // do stuff with error
@@ -44,6 +42,5 @@ router.post("*", async (req, res) => {
 });
 
 module.exports = router;
-
 
 // $cellnum = preg_replace("/[^0-9]/","",$cellnum);
