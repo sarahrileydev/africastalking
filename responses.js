44a60cdeb942a75b6a74df6115abe3ccae460923
sriley <sarahriley.dev@gmail.com>
2019-06-13 14:39:43 -0400

    test response

--- responses.js
+++ responses.js
@@ -8,11 +8,14 @@ async function getProductsInMarket(market) {
   try {
     const products = await models.findProductsForMarket(market);
     console.log("products", products);
+
     let response = "CON Choose your product";
+
     products.forEach(function(item) {
       console.log(item.product)
       response = response + item.product;
     });
+    console.log("test", response)
     return response;
   } catch (err) {
     console.log(err);
