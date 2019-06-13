1271662e1b77510edd1d72714820596d3805744a
sriley <sarahriley.dev@gmail.com>
2019-06-13 12:00:57 -0400

    add more console logs to test

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const models = require("./models");
 
 router.post("*", async (req, res) => {
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  console.log(sessionId)
+  
   let response = "";
   switch (text) {
     case "":
@@ -14,10 +14,16 @@ router.post("*", async (req, res) => {
     case "1":
       response =
         "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
+        console.log("sessionId 1", sessionId)
+        console.log("serviceCode 1", serviceCode)
+        console.log("phone Number 1", phoneNumber)
       break;
     case "1*1":
       response =
         "CON Choose your product \n 1. Eggs \n 2. Exotic Eggs \n 3. Local Eggs";
+        console.log("sessionId 2", sessionId)
+        console.log("serviceCode 2", serviceCode)
+        console.log("phone Number 2", phoneNumber)
       break;
     case "1*1*1":
       try {
@@ -29,6 +35,9 @@ router.post("*", async (req, res) => {
         newPrice.toString();
 
         response = `END Current prices for \n Eggs ${newPrice}`;
+        console.log("sessionId 3", sessionId)
+        console.log("serviceCode 3", serviceCode)
+        console.log("phone Number 3", phoneNumber)
       } catch (error) {
         console.log(error);
         // do stuff with error
