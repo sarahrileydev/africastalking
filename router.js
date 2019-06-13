830011f1ca16181beb22d4b845f3ca3eb93c9967
sriley <sarahriley.dev@gmail.com>
2019-06-13 12:01:52 -0400

    shorten cases

--- router.js
+++ router.js
@@ -9,23 +9,19 @@ router.post("*", async (req, res) => {
   switch (text) {
     case "":
       response =
-        "CON Choose your country \n 1. BTI \n 2. DRC \n 3. KEN \n 4. MWI \n 5. RWA \n 6. SSD \n 7. TZA \n 8. UGA";
-      break;
-    case "1":
-      response =
         "CON Choose your marketplace \n 1. Bujumbura \n 2. Gitega \n 3. Ngozi";
         console.log("sessionId 1", sessionId)
         console.log("serviceCode 1", serviceCode)
         console.log("phone Number 1", phoneNumber)
       break;
-    case "1*1":
+    case "1":
       response =
         "CON Choose your product \n 1. Eggs \n 2. Exotic Eggs \n 3. Local Eggs";
         console.log("sessionId 2", sessionId)
         console.log("serviceCode 2", serviceCode)
         console.log("phone Number 2", phoneNumber)
       break;
-    case "1*1*1":
+    case "1*1":
       try {
         const results = await models.findPrice("BTI", "Bujumbaru", "eggs");
         let newPrice = [];
